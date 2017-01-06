6b26e5004d441c012ce6310c197b2bcc31000de8
Franck Nijhof <f.nijhof@dealerdirect.nl>
2017-01-06 10:35:40 +0100

    Fixed autoloading and corrected the example.php

--- example.php
+++ example.php
@@ -2,7 +2,7 @@
 
 
 
-	include ( "NexmoMessage.php" );
+	include ( "vendor/autoload.php" );
 
 
 	/**
@@ -13,7 +13,7 @@
 	// Step 1: Declare new NexmoMessage.
 	$nexmo_sms = new NexmoMessage('api_key', 'api_secret');
 
-	// Step 2: Use sendText( $to, $from, $message ) method to send a message. 
+	// Step 2: Use sendText( $to, $from, $message ) method to send a message.
 	$info = $nexmo_sms->sendText( '+447234567890', 'MyApp', 'Hello!' );
 
 	// Step 3: Display an overview of the message
